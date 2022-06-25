1f8c92239a1563757d827a8cc77d8c842f02ad64
vinay supermario <vinaysupermario@gmail.com>
2022-06-25 13:10:19 +0530

    first release done, all buttons working

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ var query = "";
 var screentext = document.getElementById("calculator-screen");
 var testpara = document.getElementById("test");
 
+// AC button
 var clearbtn = document.getElementById("clear");
 clearbtn.addEventListener('click', function(){
     screentext.innerText = "0";
+    query = "";
 })
 
 function isOperator(sign)
@@ -16,10 +18,10 @@ function isOperator(sign)
     return false;
 }
 
-var numeralbtns = document.getElementsByClassName("button-numeral");
-testpara.innerText += "length is " + numeralbtns.length + '\n';
-
+// testpara.innerText += "length is " + numeralbtns.length + '\n';
 // testpara.innerText += (numeralbtns.length);
+
+// numeral buttons
 for (var i = 0; i < document.getElementsByClassName("button-numeral").length; i++)
 {
     // console.log(i + " " + document.getElementsByClassName("button-numeral")[i]);
@@ -33,6 +35,7 @@ for (var i = 0; i < document.getElementsByClassName("button-numeral").length; i+
     });
 }
 
+// +, -, /, *  buttons
 for (var i = 0; i < document.getElementsByClassName("button-operation").length; i++)
 {
     document.getElementsByClassName("button-operation")[i].addEventListener('click', function(e){
@@ -42,9 +45,10 @@ for (var i = 0; i < document.getElementsByClassName("button-operation").length; 
             query += screentext.innerText + e.target.innerText;
             screentext.innerText = "0";
         }
-    })
+    });
 }
 
+// = button
 var equalbtn = document.getElementById("button-evaluate");
 equalbtn.addEventListener('click', function(){
     query += screentext.innerText;
@@ -53,8 +57,27 @@ equalbtn.addEventListener('click', function(){
     query = "";
 });
 
+// +/- button
+var changesignbtn = document.getElementById("button-change-sign");
+changesignbtn.addEventListener('click', function(e){
+    var t = -1 * parseFloat(screentext.innerText);
+    screentext.innerText = "" + t;
+});
 
+var percentagebtn = document.getElementById("button-percentage");
+percentagebtn.addEventListener('click', function(e){
+    if (screentext.innerText == "0")
+        return;
+    var t = parseFloat(screentext.innerText)/100;
+    screentext.innerText = "" + t;
+});
 
+var decimalbtn = document.getElementById("button-decimal");
+decimalbtn.addEventListener('click', function(e){
+    if (screentext.innerText.includes('.'))
+        return;
+    screentext.innerText = screentext.innerText + '.';
+})
 
 
 
